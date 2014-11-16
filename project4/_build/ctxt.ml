b1492bf087f9776e925eeeae59a0de24413a97b8
isildur <abhirevan@gmail.com>
2014-11-16 09:24:27 -0500

    phase1 done

--- project4/_build/ctxt.ml
+++ project4/_build/ctxt.ml
@@ -13,9 +13,17 @@ failwith "dump not implemented"
 let empty : t = [] 
 
 (* try to reuse freed, otherwise create new *)
-let alloc (sym : string) (c : t) : (t * Ll.uid) =
-failwith "alloc not implemented"
+let alloc (sym : string) (c : t) : (t * Ll.uid) = 
+	try
+		let old_uid = List.assoc sym c in (c,old_uid)
+	with Not_found -> let new_uid = Ll.gen_sym () in 
+						let new_ctxt = (sym,new_uid) :: c in
+							(new_ctxt,new_uid)
+ 
 
 
 let lookup (sym : string) (c : t) : Ll.uid =
-failwith "lookup not implemented"
+	try
+		List.assoc sym c
+	with
+	| Not_found -> raise (Scope_error sym)
